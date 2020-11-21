97d82b0a7eb2ad023d9c6bc13970d5ceafce15f3
Aleksey <alex-kadykov@bk.ru>
2020-11-21 12:22:19 +0500

    task04 [Кадыков]

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,8 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Locale;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -8,7 +10,22 @@ public class Task04Main {
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - запустить программу
         // - проверить, что получилось 351.731900
+        double sum = .0d;
 
-        System.out.println("0.0");
+        InputStreamReader strRead = new InputStreamReader(System.in);
+        StringBuilder strBuf = new StringBuilder();
+        while (strRead.ready()) {
+            strBuf.appendCodePoint(strRead.read());
+        }
+
+        String[] values = strBuf.toString().split("\\s");
+        for (String value : values) {
+            try {
+                sum += Double.parseDouble(value);
+            } catch (NumberFormatException ignored) {
+            }
+        }
+
+        System.out.printf(Locale.ROOT, "%.6f\n", sum);
     }
 }
